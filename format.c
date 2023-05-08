5ad5f7f722d3ebcf5bba5bbafa98dabb2151d685
DunnowYet <93313080+DunnowYet@users.noreply.github.com>
2023-05-08 09:07:26 +0200

    Made format.c more readable

--- format.c
+++ format.c
@@ -2,12 +2,12 @@
 
 int main(){
 
-	        int ganzzahl = 25;
-		        float gleitkommazahl = 2.5;
-			        char zeichen = 'X';
-				        char zeichenkette [13] = "Hello World!";
+        int ganzzahl = 25;
+	float gleitkommazahl = 2.5;
+	char zeichen = 'X';
+	char zeichenkette [13] = "Hello World!";
 
-					        printf("Eine Ganzzahl: %i, Eine Gleitkommazahl: %.1f, ein Zeichen:\"%c\" und eine Zeichenkette \"%s\".\n", ganzzahl, gleitkommazahl, zeichen, zeichenkette);
-						        return 0;
+	printf("Eine Ganzzahl: %i, Eine Gleitkommazahl: %.1f, ein Zeichen:\"%c\" und eine Zeichenkette \"%s\".\n", ganzzahl, gleitkommazahl, zeichen, zeichenkette);
+	return 0;
 
 }
